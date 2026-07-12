efb2979817490f7f805abfb09c3895020a0a8b5e
unknown <christianyusni@gmail.com>
2026-07-12 21:09:31 +0800

    clear

--- api-translate/src/middlewares/not-found.middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from 'express';
-import { ErrorCode } from '@/constants/error-code.constant';
-import { HttpStatus } from '@/constants/http-status.constant';
-import { sendError } from '@/utils/response.util';
-
-export const notFoundMiddleware = (req: Request, res: Response): void => {
-  sendError(res, HttpStatus.NOT_FOUND, ErrorCode.NOT_FOUND, `Route ${req.method} ${req.originalUrl} not found`);
-};
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './assets/main.css'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
