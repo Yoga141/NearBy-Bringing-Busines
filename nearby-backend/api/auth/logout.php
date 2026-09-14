4ebc55cbafd8e42063b5aeff047a05b42f26d6d0
Zackmators <zackynabiliyan080922@gmail.com>
2026-09-14 17:00:55 +0800

    Create logout.php

---
+++ nearby-backend/api/auth/logout.php
@@ -0,0 +1,11 @@
+<?php // logout.php
+require_once __DIR__ . '/../../helpers/response.php';
+set_cors_headers();
+require_once __DIR__ . '/../../config/database.php';
+
+$headers = getallheaders();
+if (preg_match('/Bearer\s(\S+)/', $headers['Authorization'] ?? '', $m)) {
+    $stmt = $pdo->prepare("DELETE FROM auth_tokens WHERE token = ?");
+    $stmt->execute([$m[1]]);
+}
+json_response(['success' => true]);
